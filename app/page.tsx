e254f544d355d66262c69ddabdf5e470ba0a0723
Paulian Le <paulianle7@gmail.com>
2023-06-22 22:47:03 -0700

    Added homePage comp + add mont font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import WelcomePage from '@/components/WelcomePage'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <WelcomePage />
     </main>
   )
 }
---
+++ components/WelcomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WelcomePage() {
+  return (
+    <div className=''>
+      WelcomePage rawr!
+    </div>
+  )
+}
+
+export default WelcomePage
